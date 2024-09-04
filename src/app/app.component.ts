79d2f4242c51a80738855bd4cf173528a84487f4
Felipe <felipe.dominicheli@cxpbrasil.com.br>
2024-09-04 08:37:54 -0300

    feat: creating first component with angular cli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NewComponentComponent } from './components/new-component/new-component.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, CommonModule, NewComponentComponent],
+  template: `
+    <!-- <router-outlet /> -->
+    <h1>Curso de Angular</h1>
+    <app-new-component />
+  `,
 })
 export class AppComponent {
   title = 'meu-primeiro-projeto-latest';
